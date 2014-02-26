ec64760357accfa047c4db46219305cef2daa9d5
Hack Reactor Students <gregorylull@gmail.com>
2014-02-26 13:06:33 -0800

    Completed shared method stack

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,5 +1,34 @@
+var stackMethods = {
+  push : function(value){
+    var storage = this.storage;
+    storage[storage['length']] = value;
+    storage['length'] += 1;
+  },
+  pop : function() {
+    var storage = this.storage;
+    if(storage['length'] > 0) {
+      var transientvalue = storage[storage['length'] - 1];
+      storage['length'] -= 1;
+      return transientvalue;
+    }
+  },
+  size : function() {
+    return this.storage['length'];
+  }
+}
+
 var makeStack = function() {
   // Hey! Copy your code from src/functional/stack.js and paste it here
+  var instance = {};
+
+  // Use an object with numeric keys to store values
+  instance['storage'] = {};
+  instance.storage['length'] = 0;
+
+  for(var key in stackMethods) {
+    instance[key] = stackMethods[key];
+  }
+
+  // Implement the stackMethods below
+  return instance;
 };
-  
-var stackMethods = {};
