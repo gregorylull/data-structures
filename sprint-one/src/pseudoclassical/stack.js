9f125f1fdd0af9b87b02ec10d1f8b0f4a3e702bf
Hack Reactor Students <gregorylull@gmail.com>
2014-02-26 16:40:32 -0800

    Finished Pseudoclassical Stack

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,3 +1,27 @@
+var stackMethods = {
+  push : function(value){
+    var storage = this.storage;
+    storage[storage['length']] = value;
+    storage['length'] += 1;
+  },
+  pop : function() {
+    var storage = this.storage;
+    if(storage['length'] > 0) {
+      var transientvalue = storage[storage['length'] - 1];
+      storage['length'] -= 1;
+      return transientvalue;
+    }
+  },
+  size : function() {
+    return this.storage['length'];
+  }
+}
+
 var Stack = function() {
-  // Hey! Copy your code from src/prototypal/stack.js and paste it here
+  this.storage = {};
+  this.storage.length = 0;
 };
+
+for(var key in stackMethods) {
+  Stack.prototype[key] = stackMethods[key];
+}
