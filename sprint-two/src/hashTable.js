75102391c13302288eea336fefd0c270cfe5d771
gregorylull <gregorylull@gmail.com>
2014-02-27 23:29:36 -0800

    finished basic hashTable.js

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,16 +1,20 @@
 var HashTable = function(){
-  this._limit = 8;
-  this._storage = makeLimitedArray(this._limit);
+    this._limit = 8;
+    this._storage = makeLimitedArray(this._limit);
 };
 
 HashTable.prototype.insert = function(k, v){
-  var i = getIndexBelowMaxForKey(k, this._limit);
+    var i = getIndexBelowMaxForKey(k, this._limit);
+    this._storage.set(i, v);
 };
 
 HashTable.prototype.retrieve = function(k){
-  var i = getIndexBelowMaxForKey(k, this._limit);
+    var i = getIndexBelowMaxForKey(k, this._limit);
+    return this._storage.get(i);
 };
 
 HashTable.prototype.remove = function(k){
+    var i = getIndexBelowMaxForKey(k, this._limit);
+    this._storage.set(i, null);  
 };
 
